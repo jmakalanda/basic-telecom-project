6111e48f1488c5c357057cf23c4bf409d84d6817
jmakalanda <jmakalanda@gmail.com>
2020-04-23 10:57:51 +0200

    first commit

---
+++ src/main/java/com/example/basictelecomproject/BasicTelecomProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.basictelecomproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicTelecomProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicTelecomProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/basictelecomproject/BasicTelecomProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basictelecomproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicTelecomProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
