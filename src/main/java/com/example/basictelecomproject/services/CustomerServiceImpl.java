a7bd0df3277ae7cf6912a96f97410f08bf950faa
jmakalanda <jmakalanda@gmail.com>
2020-05-05 15:00:37 +0200

    more logging

--- src/main/java/com/example/basictelecomproject/services/CustomerServiceImpl.java
+++ src/main/java/com/example/basictelecomproject/services/CustomerServiceImpl.java
@@ -2,17 +2,19 @@ package com.example.basictelecomproject.services;
 
 import com.example.basictelecomproject.domain.Customer;
 import com.example.basictelecomproject.repository.CustomerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import javax.persistence.EntityNotFoundException;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 @Service("CustomerService")
 public class CustomerServiceImpl implements CustomerService{
+
+    Logger logger = LoggerFactory.getLogger(CustomerServiceImpl.class);
 
     @Autowired
     CustomerRepository customerRepository;
@@ -41,14 +43,14 @@ public class CustomerServiceImpl implements CustomerService{
 
         for(Customer c: allCustomers){
             calDOB.setTime(c.getDob());
-            calDOB.add(Calendar.DAY_OF_MONTH,-7);
+            logger.debug("Month and Day of the DOB is: "  + calDOB.get(Calendar.MONTH)  + " and  "+ calDOB.get(calDOB.DAY_OF_MONTH));
 
-            System.out.println("calDOB Day " + calDOB.get(calDOB.DAY_OF_MONTH) + " , now day " + calToday.get(Calendar.DAY_OF_MONTH ));
-            System.out.println("calDOB Month " + calDOB.get(calDOB.MONTH) + " , now month " + calToday.get(calToday.MONTH ));
+            calDOB.add(Calendar.DAY_OF_MONTH,-7);
+            logger.debug("Month and Day of today is: " + calToday.get(Calendar.MONTH) + " and " + calToday.get(calToday.DAY_OF_MONTH ));
 
             if ((calToday.get(Calendar.DAY_OF_MONTH ) == calDOB.get(calDOB.DAY_OF_MONTH)) && (calToday.get(Calendar.MONTH ) == calDOB.get(calDOB.MONTH))){
                 dobCustomers.add(c);
-                System.out.println("the customer has a birthdays of 7 days before" + c.getName() + " and DOB of " + c.getDob());
+                logger.debug("The Customer "+ c.getName() + " has his birthday in 7 days time on " + c.getDob());
             }
         }
         return dobCustomers;
